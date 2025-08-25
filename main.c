b72e9c72fb5f105b3666975114b16bc6eafbfef7
Mitchell Robert Ballinger <ballin20@msu.edu>
2025-08-25 19:44:55 -0400

    create buffer and retrieve stocks from file

--- main.c
+++ main.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
+#include <string.h>
+
+typedef struct StockDataPoint {
+    double price;
+    unsigned long volume;
+    time_t timestamp;
+    char ticker[10];
+} StockDataPoint;
 
 int main() {
+
+    StockDataPoint data;
+    char tickers[5][10];
+    char buffer[10];
+    FILE *fptr = fopen("StockData.txt", "r");
+    if (fptr != NULL) {
+
+        const int size = 10;
+        int i = 0;
+        while (fgets(buffer, size, fptr) != NULL && i < 5) {
+
+            char *newline_pos = strchr(buffer, '\n');
+            if (newline_pos != NULL) {
+                *newline_pos = '\0';
+            }
+
+            strcpy(tickers[i], buffer);
+
+            i++;
+        }
+    }
+
+
+
+    fclose(fptr);
+
     printf("Hello, Stock Pipeline!\n");
     return 0;
 }
