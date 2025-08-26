b521579a7450aeafee3c0c1c735a1493b32cd4b2
Mitchell Robert Ballinger <ballin20@msu.edu>
2025-08-25 21:19:08 -0400

    adding libcurl

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 typedef struct StockDataPoint {
     double price;
@@ -8,33 +9,36 @@ typedef struct StockDataPoint {
     char ticker[10];
 } StockDataPoint;
 
-int main() {
-
-    StockDataPoint data;
-    char tickers[5][10];
+int read_stock_tickers(char tickers[5][10]) {
     char buffer[10];
-    FILE *fptr = fopen("StockData.txt", "r");
+    FILE *fptr = fopen("stocks.txt", "r");
     if (fptr != NULL) {
 
         const int size = 10;
-        int i = 0;
-        while (fgets(buffer, size, fptr) != NULL && i < 5) {
+        int count = 0;
+        while (fgets(buffer, size, fptr) != NULL && count < 5) {
 
             char *newline_pos = strchr(buffer, '\n');
             if (newline_pos != NULL) {
                 *newline_pos = '\0';
             }
 
-            strcpy(tickers[i], buffer);
+            strcpy(tickers[count], buffer);
 
-            i++;
+            count++;
         }
+        fclose(fptr);
+        return count;
     }
+    return 0;
+};
 
+int main() {
 
-
-    fclose(fptr);
-
-    printf("Hello, Stock Pipeline!\n");
+    char tickers[5][10];
+    const int count = read_stock_tickers(tickers);
+    for (int i = 0; i < count; i++) {
+        printf("Tickers[%d] = %s\n", i, tickers[i]);
+    }
     return 0;
 }
